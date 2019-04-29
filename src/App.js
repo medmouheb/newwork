0a097b7761b024efb5bc23ec045bc4519a2f318a
medmouheb <mohamedmouheb@gmail.com>
2019-04-29 15:03:09 +0100

    menu

--- src/App.js
+++ src/App.js
@@ -19,36 +19,80 @@ import LoginComponenet from './Components/login sign up/login'
 import SignupComponenet from './Components/login sign up/signUP'
 import QuestionFormat from './Components/genericForm/genericform'
 import NewNAvbar from './Components/newnav/newNav'
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
 class App extends Component {
 
   render() {
     const { menu,products,footer,discountProduct ,sidemenu,ZoomIMG,BigSaleSRC,CarouselPhotos,ProductImagelist} = this.props
  
     return (
+      <Router>
       <div className="App ">
         <Container>
         <NewNAvbar/>
         <NavBar data={menu}/>
+        <Link to="/login">   login</Link>
+        <Link to="/sidebar">   sidebar</Link>
+        <Link to="/signup">    signup</Link>
+        <Link to="/goodzoom">     goodzoom</Link>
+        <Link to="/Simpleproduct">     Simpleproduct</Link>
+        <Link to="/footer">    footer</Link>
+        <Link to="/bigsale">    bigsale</Link>
+        <Link to="/discount">    discount</Link>
+        <Link to="/carsoulProduct">    carsoulProduct</Link>
+        <Link to="/questionform">    questionform</Link>
+
         <Row>
           {window.screen.width >=1000?<Col sm><Filter/></Col>:""}
           <Col sm={8}>
           {window.screen.width <=1000?<Col sm><Filter/></Col>:""}
-          <LoginComponenet/>
-          <SignupComponenet/>
-            <Sidebar data={sidemenu}/>
-            <GoodZoom data={ProductImagelist}/>
-            <SimpleProduct data={products}/>
-            <Footer data={footer}/>
-            <BigSale data={BigSaleSRC}/>
-            <DiscountList data={discountProduct}/>
-            <CarousalProduct data={discountProduct}/>
-            <CarouselPhoto data={CarouselPhotos}/>
-            <QuestionFormat/>
+          <Route
+  path='/login'
+  component={() => <LoginComponenet/>}
+/>
+<Route
+  path='/sidebar'
+  component={() => <Sidebar data={sidemenu}/>}
+/>
+<Route
+  path='/signup'
+  component={() => <SignupComponenet/>}
+/>
+<Route
+  path='/goodzoom'
+  component={() => <GoodZoom data={ProductImagelist}/>}
+/>
+<Route
+  path='/Simpleproduct'
+  component={() => <SimpleProduct data={products}/>}
+/>
+<Route
+  path='/footer'
+  component={() => <Footer data={footer}/>}
+/>
+<Route
+  path='/bigsale'
+  component={() => <BigSale data={BigSaleSRC}/>}
+/>
+<Route
+  path='/discount'
+  component={() => <DiscountList data={discountProduct}/>}
+/>
+<Route
+  path='/carsoulProduct'
+  component={() => <CarousalProduct data={discountProduct}/>}
+/>
+<Route
+  path='/questionform'
+  component={() => <QuestionFormat/>}
+/>
           </Col> 
           </Row>
         </Container>
         
  </div>
+ </Router>
     );
   }
 }
